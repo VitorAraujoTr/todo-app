5ed11721fc802b4fd38266390d5f3dbcca6313d6
vitor.araujo <vitor.araujo@isgsa.com.br>
2019-07-15 18:22:39 -0300

    adicionada a função de procurar pela descrição e limpar campo de inserção/busca

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -7,9 +7,13 @@ export default props => {
         const list = props.list || []
         return list.map(todo => (
             <tr key={todo._id} >
-                <td>{todo.description}</td>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(todo)}>
+                    <IconButton style='sucess' icon='check' hide={todo.done} onClick={() => props.handleMarkAsDone(todo)}>
+                    </IconButton>
+                    <IconButton style='warning' icon='undo' hide={!todo.done} onClick={() => props.handleMarkAsPending(todo)}>
+                    </IconButton>
+                    <IconButton style='danger' icon='trash-o' hide={!todo.done} onClick={() => props.handleRemove(todo)}>
                     </IconButton>
                 </td>
             </tr>
@@ -23,7 +27,7 @@ export default props => {
                     <th>
                         Descrição
                     </th>
-                    <th>
+                    <th className='tableActions'>
                         Ações
                     </th>
                 </tr>
